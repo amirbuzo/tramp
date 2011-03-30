a851f6c132c4f2e8fb61c70bdb8a0dd6f2019ded
Dylan Greene <dylang@gmail.com>
2011-03-30 13:29:33 -0400

    adding tests

--- test/tramp.js
+++ test/tramp.js
@@ -1,25 +1,55 @@
+/*
+    Use nodeunit to run these tests.
+    All should pass.
+ */
+
 var Tramp = require('../index');
 
 exports.justText = function(test){
     test.expect(1);
-    Tramp.translate({input: 'English', output: 'French'}, {property: 'test.1', message: 'This is a test with just text.'}, function(result){
-        test.equals(result, 'test.1=Ceci est un test avec juste le texte.');
+    Tramp.translate({input: 'English', output: 'French'}, {property: 'test', message: 'This is a test with just text.'}, function(result){
+        test.equals(result, 'test=Ceci est un test avec juste le texte.');
         test.done();
     });
 };
 
 exports.textAndHTML = function(test){
     test.expect(1);
-    Tramp.translate({input: 'English', output: 'French'}, {property: 'test.1', message: '<div class="test happy good">This is a test <strong>with some html</strong>.</div>'}, function(result){
-        test.equals(result, 'test.1=<div class="test happy good"> Ceci est un test <strong>avec quelques html.</strong> </div>');
+    Tramp.translate({input: 'English', output: 'French'}, {property: 'test', message: '<div class="test happy good">This is a test <strong>with some html</strong>.</div>'}, function(result){
+        test.equals(result, 'test=<div class="test happy good"> Ceci est un test <strong>avec quelques html.</strong> </div>');
         test.done();
     });
 };
 
+exports.variables = function(test){
+    test.expect(1);
+    Tramp.translate({input: 'English', output: 'French'}, {property: 'test', message: 'I would like {0} to go to {1} on {2} with {3}.'}, function(result){
+        test.equals(result, 'test=Je voudrais {0} pour aller à {1} le {2} avec {3}.');
+        test.done();
+    });
+};
 
-// {0}, {1}
-//other langage to english
-// comment
-// blank lines
-// missing options
+exports.otherLanguageToEnglish = function(test){
+    test.expect(1);
+    Tramp.translate({input: 'French', output: 'English'}, {property: 'test', message: 'Ceci est un test.'}, function(result){
+        test.equals(result, 'test=This is a test.');
+        test.done();
+    }); 
+};
+
+exports.comment = function(test){
+    test.expect(1);
+    Tramp.translate({input: 'English', output: 'French'}, {comment: '# This is a comment.'}, function(result){
+        test.equals(result, '# This is a comment.');
+        test.done();
+    });
+};
+
+exports.blankMessage = function(test){
+    test.expect(1);
+    Tramp.translate({input: 'English', output: 'French'}, {property: 'test', message: ''}, function(result){
+        test.equals(result, 'test=');
+        test.done();
+    });
+};
 
